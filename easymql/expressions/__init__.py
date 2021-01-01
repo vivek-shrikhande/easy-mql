a668b3335692bd5712f9469be4e2e70f29606c51
vivek-shrikhande <65864138+vivek-shrikhande@users.noreply.github.com>
2021-01-01 22:34:39 +0530

    feat/test: Add grammars/tests for boolean expressions

--- easymql/expressions/__init__.py
+++ easymql/expressions/__init__.py
@@ -1,10 +1,11 @@
 from easymql import Grammar
 from easymql.datatypes import DataType
 from easymql.expressions.arithmetic import ArithmeticExpression
+from easymql.expressions.boolean import BooleanExpression
 from easymql.proxies import expression_proxy
 
 
 class Expression(Grammar):
 
     grammar = expression_proxy
-    grammar <<= ArithmeticExpression() | DataType()
+    grammar <<= ArithmeticExpression() | BooleanExpression() | DataType()
