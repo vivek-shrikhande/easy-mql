7b4debb56c1b8fb64bdbe1126766d27e6e8c67c4
vivek-shrikhande <65864138+vivek-shrikhande@users.noreply.github.com>
2021-01-01 19:48:29 +0530

    fix: Simplify ExpressionAction to set args as list
    Irrespective of the number of expression arguments, always send
    them as a list. This will avoid two different logics for single
    argument expression vs mutliple argument expression.

--- tests/expressions/test_arithmetic.py
+++ tests/expressions/test_arithmetic.py
@@ -10,7 +10,8 @@ class TestArithmeticExpression:
         cls.exp = Expression()
 
     def test_abs(self):
-        assert self.exp.parse('ABS(1)') == [{'$abs': 1}]
+        assert self.exp.parse('ABS(1)') == [{'$abs': [1]}]
+        assert self.exp.parse('ABS([1])') == [{'$abs': [[1]]}]
         with raises(ParseException):
             self.exp.parse('ABS(1, 2)')
 
@@ -21,7 +22,7 @@ class TestArithmeticExpression:
             self.exp.parse('ADD(1)')
 
     def test_ceil(self):
-        assert self.exp.parse('CEIL(1)') == [{'$ceil': 1}]
+        assert self.exp.parse('CEIL(1)') == [{'$ceil': [1]}]
         with raises(ParseException):
             self.exp.parse('CEIL(1, 2)')
 
@@ -33,17 +34,17 @@ class TestArithmeticExpression:
             self.exp.parse('DIVIDE(1)')
 
     def test_exp(self):
-        assert self.exp.parse('EXP(1)') == [{'$exp': 1}]
+        assert self.exp.parse('EXP(1)') == [{'$exp': [1]}]
         with raises(ParseException):
             self.exp.parse('EXP(1, 2)')
 
     def test_floor(self):
-        assert self.exp.parse('FLOOR(1)') == [{'$floor': 1}]
+        assert self.exp.parse('FLOOR(1)') == [{'$floor': [1]}]
         with raises(ParseException):
             self.exp.parse('FLOOR(1, 2)')
 
     def test_ln(self):
-        assert self.exp.parse('LN(1)') == [{'$ln': 1}]
+        assert self.exp.parse('LN(1)') == [{'$ln': [1]}]
         with raises(ParseException):
             self.exp.parse('LN(1, 2)')
 
@@ -55,7 +56,7 @@ class TestArithmeticExpression:
             self.exp.parse('LOG(1)')
 
     def test_log10(self):
-        assert self.exp.parse('LOG10(1)') == [{'$log10': 1}]
+        assert self.exp.parse('LOG10(1)') == [{'$log10': [1]}]
         with raises(ParseException):
             self.exp.parse('LOG10(1, 2)')
 
@@ -80,13 +81,13 @@ class TestArithmeticExpression:
             self.exp.parse('POW(1)')
 
     def test_round(self):
-        assert self.exp.parse('ROUND(1)') == [{'$round': 1}]
+        assert self.exp.parse('ROUND(1)') == [{'$round': [1]}]
         assert self.exp.parse('ROUND(1, 2)') == [{'$round': [1, 2]}]
         with raises(ParseException):
             self.exp.parse('ROUND(1, 2, 3)')
 
     def test_sqrt(self):
-        assert self.exp.parse('SQRT(1)') == [{'$sqrt': 1}]
+        assert self.exp.parse('SQRT(1)') == [{'$sqrt': [1]}]
         with raises(ParseException):
             self.exp.parse('SQRT(1, 2)')
 
@@ -98,7 +99,7 @@ class TestArithmeticExpression:
             self.exp.parse('SUBTRACT(1)')
 
     def test_trunc(self):
-        assert self.exp.parse('TRUNC(1)') == [{'$trunc': 1}]
+        assert self.exp.parse('TRUNC(1)') == [{'$trunc': [1]}]
         assert self.exp.parse('TRUNC(1, 2)') == [{'$trunc': [1, 2]}]
         with raises(ParseException):
             self.exp.parse('TRUNC(1, 2, 3)')
